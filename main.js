b2ba782f17d3e2e7c650c9cf0583365b0522a496
Pranjal-Pandey245 <81172387+Pranjal-Pandey245@users.noreply.github.com>
2022-05-15 10:24:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music1="";
 music2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload(){
     music1= loadSound("music.mp3");
@@ -13,8 +17,30 @@ function setup(){
 
     video= createCapture(VIDEO);
     video.hide();
+
+    posenet= ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotResults);
 }
 
 function draw(){
     image(video, 0, 0, 500, 400);
+}
+
+function modelLoaded(){
+    console.log("Modal Loaded!")
+}
+
+function gotResults(results){
+    if(results.length>0){
+        leftWristX= results[0].pose.leftWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+        rightWristX= results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+
+
+        console.log("Left Wrist X- "+ leftWristX);
+        console.log("Left Wrist Y- "+ leftWristY);
+        console.log("Right Wrist X- "+ rightWristX);
+        console.log("Right Wrist Y- "+ rightWristY);
+    }
 }
